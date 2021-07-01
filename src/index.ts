d1d6a7d81ccc6bceb89c05863b7bf70aec1a389a
evilebottnawi <sheo13666q@gmail.com>
2021-07-01 16:05:07 +0300

    fix: usage web workers with ts

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,18 @@
 import { wrap } from 'comlink';
-
 import type { WorkerApi } from './workers/foo.worker';
 
 const heading = document.createElement('H1');
+
 heading.innerHTML = 'Hello world!';
 
 document.body.appendChild(heading);
 
-
 const test = async () => {
-  const workerUrl = new URL('./workers/foo.worker', import.meta.url);
-  console.log('workerUrl', workerUrl);
-  
-  const worker = new Worker(workerUrl);
-  
+  const worker = new Worker(new URL('./workers/foo.worker.ts', import.meta.url));
   const service = wrap<WorkerApi>(worker);
-  
   const result = await service.someSillyFunction();
-  
-
   const paragraph = document.createElement('P');
+  
   paragraph.innerHTML = `This number came from the worker: ${result}`;
   document.body.appendChild(paragraph);
 };

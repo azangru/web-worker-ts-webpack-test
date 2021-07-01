26a7d9a4d001569db68fe1f0a0d59b9e3b812ee7
Alexander Akait <4567934+alexander-akait@users.noreply.github.com>
2021-07-01 19:02:48 +0300

    fix: usage web workers with ts (#1)

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.ts',
-  mode: 'development',
   module: {
     rules: [
       {
@@ -16,18 +15,9 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    assetModuleFilename: (pathData) => {
-      const { filename } = pathData;
-
-      if (filename.endsWith('.ts')) {
-        return '[name]-[hash].js'
-      } else {
-        return '[hash][ext][query]';
-      }
-    }
   },
   plugins: [new HtmlWebpackPlugin()],
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: ['.tsx', '.ts', '...'],
   }
 };
